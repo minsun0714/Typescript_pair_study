ffe6749389670160f471dfd515c824592ec6391e
jasmine0714 <woooooooow22@gmail.com>
2023-04-26 17:36:02 +0900

    🎨 type Meal에서 type Year로 alias 예시 변경

--- basic-type/이민선/2. alias.ts
+++ basic-type/이민선/2. alias.ts
@@ -3,6 +3,12 @@
    * alias
    * 기존 타입에 새로운 이름을 부여하여 사용하는 방식
    */
+
+  // 2023이라는 숫자로 지정한 Year type을 year라는 변수에 지정하면, 2023만 할당 가능
+  type Year = 2023;
+  const year: Year = 2023;
+
+  // number만 원소로 둘 수 있는 NumArray type 생성 가능
   type NumArray = number[];
   const numberContainer: NumArray = [1, 2, 3, 4, 5];
 
@@ -17,7 +23,4 @@
     title: "Gravity",
     cast: ["Sandra Bullock", "George Clooney", "Ed Harris"],
   };
-
-  type Meal = "rice";
-  const meal: Meal = "rice";
 }
