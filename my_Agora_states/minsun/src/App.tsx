d85504f15a2fd74ace959bc123ca878f4793e405
jasmine0714 <woooooooow22@gmail.com>
2023-05-07 14:12:18 +0900

    GET 요청한 정보 렌더링

--- my_Agora_states/minsun/src/App.tsx
+++ my_Agora_states/minsun/src/App.tsx
@@ -1,9 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 
+const Board = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid green;
+  border-radius: 20px;
+  height: 70px;
+  margin: 20px;
+`;
+
 function App() {
-  return <div></div>;
+  type Discussions = {
+    answer: {
+      author: string;
+      avatarURL: string;
+      bodyHTML: string;
+      createdAt: string;
+      id: string;
+      url: string;
+    };
+    author: string;
+    avatarURL: string;
+    bodyHTML: string;
+    createdAt: string;
+    id: number;
+    url: string;
+    title: string;
+    updatedAt: string;
+  };
+  const [discussions, setDiscussions] = useState<Discussions[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/discussions/")
+      .then((response) => {
+        setDiscussions(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <main>
+      <ul>
+        {discussions.map((discussion) => (
+          <Card key={discussion.id}>
+            <li>
+              <span>{discussion.title}</span>
+              <br />
+              <span>{discussion.author}</span>
+            </li>
+          </Card>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
