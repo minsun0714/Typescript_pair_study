daa06327d325fcdceb514f14c7559f4ed76a6caa
jasmine0714 <woooooooow22@gmail.com>
2023-05-07 20:23:29 +0900

    🚚 Discussions 폴더로 이동

---
+++ my_Agora_states/minsun/src/components/Discussions/Discussions.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Pagination from "../Paginations/Pagination";
+
+function Discussions() {
+  const Main = styled.main`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const Board = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `;
+  return (
+    <Main>
+      <Board>
+        <input placeholder='이름을 입력하세요'></input>
+        <textarea placeholder='질문을 입력하세요'></textarea>
+        <button>제출</button>
+      </Board>
+      <Pagination />
+    </Main>
+  );
+}
+export default Discussions;
