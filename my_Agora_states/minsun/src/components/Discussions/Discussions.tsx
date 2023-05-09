f4bf09bd7b459f35368a86d45c5b6a1daeb0ec62
jasmine0714 <woooooooow22@gmail.com>
2023-05-09 15:19:27 +0900

    🎨 제출 버튼 클릭 시 reducer로 제목/이름 추가하도록 구현

--- my_Agora_states/minsun/src/components/Discussions/Discussions.tsx
+++ my_Agora_states/minsun/src/components/Discussions/Discussions.tsx
@@ -1,26 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Pagination from "../Paginations/Pagination";
+import { useDispatch, useSelector } from "react-redux";
+import { createDiscussion } from "../../store/store";
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Board = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+type Discussions = {
+  answer: {
+    author: string;
+    avatarURL: string;
+    bodyHTML: string;
+    createdAt: string;
+    id: string;
+    url: string;
+  };
+  author: string;
+  avatarURL: string;
+  bodyHTML: string;
+  createdAt: string;
+  id: number;
+  url: string;
+  title: string;
+  updatedAt: string;
+};
 
 function Discussions() {
-  const Main = styled.main`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-  `;
+  const [userName, setUserName] = useState("");
+  const [discussionTitle, setDiscussionTitle] = useState("");
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
-  const Board = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  `;
+  const onChangeUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+    setUserName(event.currentTarget.value);
+  };
+
+  const onChangeDiscussionTitle = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    console.log(event.currentTarget.value);
+    setDiscussionTitle(event.currentTarget.value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(
+      createDiscussion({
+        answer: {
+          author: userName,
+          avatarURL: "hi",
+          bodyHTML: "hi",
+          createdAt: "hi",
+          id: Date.now(),
+          url: "hi",
+        },
+        author: userName,
+        avatarURL: "hi",
+        bodyHTML: "hi",
+        createdAt: "hi",
+        id: Date.now(),
+        url: "hi",
+        title: discussionTitle,
+        updatedAt: "hi",
+      })
+    );
+    setUserName("");
+    setDiscussionTitle("");
+  };
+
   return (
     <Main>
-      <Board>
-        <input placeholder='이름을 입력하세요'></input>
-        <textarea placeholder='질문을 입력하세요'></textarea>
+      <Board onSubmit={onSubmit}>
+        <input
+          placeholder='이름을 입력하세요'
+          onChange={onChangeUserName}
+        ></input>
+        <textarea
+          placeholder='질문을 입력하세요'
+          onChange={onChangeDiscussionTitle}
+        ></textarea>
         <button>제출</button>
       </Board>
       <Pagination />
