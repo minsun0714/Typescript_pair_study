3d23e6028ec6db074e77290f7c7daa35e5a04e6c
jasmine0714 <woooooooow22@gmail.com>
2023-05-09 14:50:08 +0900

    ✨ Redux slice reducer 객체 생성

---
+++ my_Agora_states/minsun/src/store/store.tsx
@@ -0,0 +1,38 @@
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type Discussions = {
+  answer: {
+    author: string;
+    avatarURL: string;
+    bodyHTML: string;
+    createdAt: string;
+    id: string;
+    url: string;
+  };
+  author: string;
+  avatarURL: string;
+  bodyHTML: string;
+  createdAt: string;
+  id: number;
+  url: string;
+  title: string;
+  updatedAt: string;
+};
+
+const initialDiscussionState: Discussions[] = [];
+
+export const discussion = createSlice({
+  name: "discussionReducer",
+  initialState: initialDiscussionState,
+  reducers: {
+    createDiscussion: (
+      state: Discussions[],
+      action: PayloadAction<Discussions>
+    ) => {
+      return [action.payload, ...state];
+    },
+  },
+});
+
+export const store = configureStore({ reducer: discussion.reducer });
+export const { createDiscussion } = discussion.actions;
