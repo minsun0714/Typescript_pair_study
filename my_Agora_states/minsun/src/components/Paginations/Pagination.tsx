126bba263d4a21aae0b92fefcd16a1cca06a1ec6
jasmine0714 <woooooooow22@gmail.com>
2023-05-12 23:36:05 +0900

    🐛 import React

--- my_Agora_states/minsun/src/components/Paginations/Pagination.tsx
+++ my_Agora_states/minsun/src/components/Paginations/Pagination.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PageBtns from "./PageBtns";
 import styled from "styled-components";
