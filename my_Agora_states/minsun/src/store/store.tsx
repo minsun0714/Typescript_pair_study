ba1980692273abd9e2c2612ec8c4c63e277194b4
jasmine0714 <woooooooow22@gmail.com>
2023-05-19 15:45:35 +0900

    ✨ local storage에 discussions 데이터 저장

--- my_Agora_states/minsun/src/store/store.tsx
+++ my_Agora_states/minsun/src/store/store.tsx
@@ -19,16 +19,32 @@ export type Discussion = {
   updatedAt: string | null;
 };
 
-const initialDiscussionState: Discussion[] = [];
+const initialDiscussionStateLocalStorage: Discussion[] = [];
+
+for (let key in localStorage) {
+  const value = localStorage.getItem(key);
+  if (value) {
+    const parsedValue = JSON.parse(value);
+    if (parsedValue.author) {
+      initialDiscussionStateLocalStorage.push(parsedValue);
+    }
+  }
+}
+
+initialDiscussionStateLocalStorage.sort((a, b) => b.id - a.id);
 
 export const discussion = createSlice({
   name: "discussionReducer",
-  initialState: initialDiscussionState,
+  initialState: initialDiscussionStateLocalStorage,
   reducers: {
     createDiscussion: (
       state: Discussion[],
       action: PayloadAction<Discussion>
     ) => {
+      localStorage.setItem(
+        String(action.payload.id),
+        JSON.stringify(action.payload)
+      );
       return [action.payload, ...state];
     },
   },
