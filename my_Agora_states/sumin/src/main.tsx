5a4d5b16b012c590c389ab982cee89e54a2e6a6e
tnals9707 <zzummin9721@gmail.com>
2023-05-23 16:38:38 +0900

    ✨ header 작성

--- my_Agora_states/sumin/src/main.tsx
+++ my_Agora_states/sumin/src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Main = () => {
-  return (
-    <div></div>
-  )
+    return (
+        <>
+            <header className='title'>
+                <h1>✦ My Agora States ✦</h1>
+            </header>
+            <section className='main'>
+            </section>
+        </>
+    )
 }
 
 export default Main
