ae57034e7dd709c69d16f0bd130c10c9f28a87ba
tnals9707 <zzummin9721@gmail.com>
2023-05-23 16:55:26 +0900

    💄 스타일 추가, import InputForm, DiscussionForm

--- my_Agora_states/sumin/src/App.tsx
+++ my_Agora_states/sumin/src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import Main from './main';
+import styled from 'styled-components';
+import InputForm from './components/inputForm';
+import DiscussionForm from './components/discussions';
+
+const Body = styled.body`
+  display: flex;
+  flex-direction: column;
+`
+
+const Header = styled.header`
+  width: 100%;
+`
+
+const FormWapper = styled.section`
+  display: flex;
+  flex-direction: row;
+`
 
 function App() {
   return (
     <>
-      <Main />
+      <Body>
+        <Header>
+          <h1>✦ My Agora States ✦</h1>
+        </Header>
+        <FormWapper>
+          <InputForm />
+          <DiscussionForm />
+        </FormWapper>
+      </Body>
     </>
   );
 }
