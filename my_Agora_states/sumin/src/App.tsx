e896beb7d2e1b7352e7a9bf1e743f366d4f7c099
tnals9707 <zzummin9721@gmail.com>
2023-05-28 23:22:02 +0900

    💄 globalstyled 추가

--- my_Agora_states/sumin/src/App.tsx
+++ my_Agora_states/sumin/src/App.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 import InputForm from './components/inputForm';
 import DiscussionForm from './components/discussions';
+import GlobalStyle from 'styled/globalStyled';
 
 const Body = styled.body`
+  padding-top: 3rem;
   display: flex;
+  align-items: center;
   flex-direction: column;
+  background-color: rgb(242,239,237);
 `
 
 const Header = styled.header`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const FormWapper = styled.section`
@@ -20,6 +27,7 @@ const FormWapper = styled.section`
 function App() {
   return (
     <>
+    <GlobalStyle />
       <Body>
         <Header>
           <h1>✦ My Agora States ✦</h1>
--- my_Agora_states/sumin/src/components/discussions.tsx
+++ my_Agora_states/sumin/src/components/discussions.tsx
@@ -4,7 +4,7 @@ import InputForm from './inputForm';
 import { dataListMap } from './dummyData';
 import { Discussions } from './dummyData';
 
-const DiscussionWrapper = styled.ul`
+const DiscussionWrapper = styled.div`
     /* background-color: rgb(2,114,104,0.2); */
     border-radius: 10px;
     width: 54rem;
@@ -13,8 +13,23 @@ const DiscussionWrapper = styled.ul`
     padding: 20px;
 `
 
-const DiscussionContainer = styled.li`
-    background-color: rgb(133,114,104);
+const DiscussionContainer = styled.ul`
+    /* background-color: rgb(133,114,104);
+    border-radius: 10px;
+    width: 32%;
+    height: 185px;
+    padding: 15px;
+    margin-bottom: 13px;
+    display: flex;
+    flex-direction: column;
+    box-shadow: rgba(255, 255, 255, 0.235) 3px 3px 6px 0px inset, 
+    rgba(101, 83, 73, 0.5) -3px -3px 6px 1px inset;
+    transition-duration: 0.5s; */
+`
+const AvatarAnswered = styled.li`
+    border: black 1px solid;
+    margin-bottom: 10px;
+    background-color: rgb(255, 255, 255);
     border-radius: 10px;
     width: 32%;
     height: 185px;
@@ -27,13 +42,35 @@ const DiscussionContainer = styled.li`
     transition-duration: 0.5s;
 `
 
+const ImgStyle = styled.img`
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+`
+
+const Answered = styled.div`
+background-color: #9cbdbd;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+`
+
 function Discussion() {
 
-    return (
+     return (
         <>
-            <DiscussionWrapper>
+            <DiscussionWrapper > 
                 <DiscussionContainer>
-                    {dataListMap.map((data: Discussions) => <img src={data.avatarUrl}/>)}
+                    {dataListMap.map((data: Discussions, index) => (
+                        <AvatarAnswered key={index}>
+                            <Answered>
+                                <p><ImgStyle src={data.avatarUrl} /></p>
+                                <p>{data.answer? '✦' : '✧'}</p>
+                            </Answered>
+                            <p>{data.title}</p>
+                            <p>{data.author}/{data.createdAt}</p>
+                        </AvatarAnswered>
+                    ))}
                 </DiscussionContainer>
             </DiscussionWrapper>
         </>
